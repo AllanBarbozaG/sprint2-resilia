cd9c15ee4e6539ffaee1c6a78a46bf1e39c2e3dd
allanbarbozag <allpatrick.b@gmail.com>
2022-07-31 18:44:18 -0300

    adiciona levnte de servidor com o express.

---
+++ app.js
@@ -0,0 +1,15 @@
+import express from "express";
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
+const port = process.env.PORT || 3000;
+
+const app = express();
+
+app.listen(port, () => {
+  console.log(`Servidor online no endereço http://localhost:${port}`)
+})
+
+app.use(express.json())
+
